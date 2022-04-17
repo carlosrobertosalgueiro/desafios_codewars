c66e13db10e826baa07112bfd152da877c3e8540
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-04-17 00:14:20 -0300

    finished

--- lib/fundamentals/opposites.ex
+++ lib/fundamentals/opposites.ex
@@ -11,4 +11,5 @@ end
 # Timmy e Sarah pensam que estão apaixonados, mas perto de onde moram, eles só saberão
 # quando colherem uma flor cada. Se uma das flores tiver um número par de pétalas e a
 # outra tiverum número ímpar de pétalas, significa que eles estão apaixonados.
-# Escreva uma função que pegue o número de pétalas de cada flor e retorne true se elas estiverem apaixonados
+# Escreva uma função que pegue o número de pétalas de cada flor e retorne true se elas
+# estiverem apaixonados e false se não estiverem apaixonados.
